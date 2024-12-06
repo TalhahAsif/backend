eea3ea9061f9396c053b4e597e57344597437eab
Talha Asif <98359013+TalhahAsif@users.noreply.github.com>
2024-12-07 02:10:49 +0500

    commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello World");
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  console.log("req==>", req);
+
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => console.log(`Server is running on localhost:${PORT}`));
+
+
